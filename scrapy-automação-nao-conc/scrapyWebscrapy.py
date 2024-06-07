0732ae1b9ac83d277081400ff29320877a6c75d7
Jean Carlos <jeancsp2003@gmail.com>
2024-06-07 13:56:04 -0300

    mais

---
+++ scrapy-automação-nao-conc/scrapyWebscrapy.py
@@ -0,0 +1,9 @@
+#serve para pegar informações de sites automáticamente
+#scrapy startproject (imdb250) - nome do arquivo/projeto - direto no terminal
+
+#scrapy genspider imdb(nome) imdb.com(link/dominio)
+#scrapy shell - comando para testar em uma area de teste
+#fetch('endereço do site')
+  #pode ocorrer erros nessa parte, só o 200 passa
+ # response - serve para pergunta e obter uma resposta do site a partir do codigo pegado noq eu quero obter no site
+# .get() - pegar o primeiro indice de uma lista 
