986baf04d12f534cca72bd51fcef01dcb5b7c5df
Jean Carlos <jeancsp2003@gmail.com>
2024-06-18 15:14:56 -0300

    kivy
    faltou so aprender a fazer o deploy

---
+++ kivy/app.py
@@ -0,0 +1,30 @@
+#biblioteca para criação de apps
+#importa o App, depois o builder(GUI(interface(ex:tela.Kv)))
+#criar o aplicativo
+#criar a função builder
+
+from kivy.app import App
+from kivy.lang import Builder
+import requests
+
+gui = Builder.load_file('tela.Kv')
+
+class MeuAplicativo(App):
+    def build(self):
+        return gui
+    
+    def on_start(self):  #vai rodar essa função sempre q o app inicializar
+        cotacao_dolar = self.pegar_moeda("USD")
+        self.root.ids["moeda1"].text = f"dolar {self.pegar_moeda('USD')}"
+        self.root.ids["moeda2"].text = f"euro {self.pegar_moeda('EUR')}"
+    
+    def pegar_moeda(self, moeda):
+        link = f"https://economia.awesomeapi.com.br/last/{moeda}-BRL"
+        requisi = requests.get(link)
+        dic_requisicap = requisi.json()
+        cotacao = dic_requisicap[f"{moeda}BRL"]["bid"]
+        return cotacao
+    
+
+
+MeuAplicativo().run()
